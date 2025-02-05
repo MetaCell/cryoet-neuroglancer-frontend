48ff9fb0de1b515b3ae45ddfda75c3239f10b7c9
aranega <vincent.aranega@gmail.com>
2025-02-05 05:22:39 -0600

    Test alternative compression algos

--- applications/cryoet-neuroglancer/frontend/src/utils.ts
+++ applications/cryoet-neuroglancer/frontend/src/utils.ts
@@ -1,4 +1,10 @@
 import pako from "pako";
+import {
+  compressToEncodedURIComponent,
+  compressToUint8Array,
+  decompressFromEncodedURIComponent,
+  decompressFromUint8Array,
+} from "lz-string";
 
 function hash2jsonString(hash: string): string {
   if (hash.startsWith("#!")) {
@@ -41,22 +47,54 @@ export function hashIsCompressed(hash: string): boolean {
   return !hashIsUncompressed(hash);
 }
 
+function gzipCompression(s: string): string {
+  const gzipHash = pako.gzip(s);
+  const base64UrlFragment = btoa(String.fromCharCode.apply(null, gzipHash));
+  return b64Tob64Url(base64UrlFragment);
+}
+
+function gzipDecompression(s: string): string {
+  const base64Hash = b64UrlTo64(s);
+  const gzipedHash = Uint8Array.from(atob(base64Hash), (c) => c.charCodeAt(0));
+  return new TextDecoder().decode(pako.ungzip(gzipedHash));
+}
+
+// function lzstringCompression(s: string): string {
+//   return compressToEncodedURIComponent(s);
+// }
+
+// function lzstringdecompression(s: string): string {
+//   return decompressFromEncodedURIComponent(s);
+// }
+
+// function lzstringCompression2(s: string): string {
+//   const uintArray = compressToUint8Array(s);
+//   const base64UrlFragment = btoa(String.fromCharCode.apply(null, uintArray));
+//   return b64Tob64Url(base64UrlFragment);
+// }
+
+// function lzstringDecompression2(s: string): string {
+//   const base64Hash = b64UrlTo64(s);
+//   const gzipedHash = Uint8Array.from(atob(base64Hash), (c) => c.charCodeAt(0));
+//   return decompressFromUint8Array(gzipedHash);
+// }
+
 export function compressHash(hash: string): string {
+  console.time("compression");
   if (hashIsCompressed(hash)) {
     return hash;
   }
-  const gzipHash = pako.gzip(hash2jsonString(hash));
-  const base64UrlFragment = btoa(String.fromCharCode.apply(null, gzipHash));
-  const newHash = `#!${b64Tob64Url(base64UrlFragment)}`;
-  return newHash;
+  const compressedHash = gzipCompression(hash2jsonString(hash));
+  console.timeEnd("compression");
+  return `#!${compressedHash}`;
 }
 
 export function decompressHash(hash: string): string {
   if (hashIsUncompressed(hash)) {
     return hash;
   }
-  const base64Hash = b64UrlTo64(hash2jsonString(hash));
-  const gzipedHash = Uint8Array.from(atob(base64Hash), (c) => c.charCodeAt(0));
-  const hashFragment = new TextDecoder().decode(pako.ungzip(gzipedHash));
+  console.time("decompression");
+  const hashFragment = gzipDecompression(hash2jsonString(hash));
+  console.timeEnd("decompression");
   return `#!${hashFragment}`;
 }
